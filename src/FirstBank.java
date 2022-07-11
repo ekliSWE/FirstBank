8781012121fd84d240d83ba05a45275431d6bfbc
ekliSWE <105136840+ekliSWE@users.noreply.github.com>
2022-07-11 16:41:07 -0400

    Updated

---
+++ src/FirstBank.java
@@ -0,0 +1,16 @@
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Account ek = new Account("Ek li", "A00001");
+		
+		
+		Account Stan = new Account("Stan", "B00002");
+		Stan.showMenu();
+		
+		
+	}
+
+}
